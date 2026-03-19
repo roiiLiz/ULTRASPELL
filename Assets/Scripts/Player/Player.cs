196c0d1e25cd03f3c3508b98f78de2c7f39db884
Liz Thompson <149344267+roiiLiz@users.noreply.github.com>
2026-03-18 20:51:03 -0400

    (feat) Added CameraController.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour {
     [Header("Input Actions")]
     [SerializeField] private InputActionReference movementInput;
     [SerializeField] private InputActionReference jumpInput;
+    [SerializeField] private InputActionReference lookInput;
     [SerializeField] private InputActionReference lightAttackInput;
     [SerializeField] private InputActionReference heavyAttackInput;
     [SerializeField] private InputActionReference weaponSwapInput;
@@ -15,6 +16,8 @@ public class Player : MonoBehaviour {
     private MovementComponent movement;
     private JumpComponent jump;
     private GravityComponent gravity;
+    private CameraController camComponent;
+    private Camera cam;
 
     Vector3 finalMove;
 
@@ -24,9 +27,22 @@ public class Player : MonoBehaviour {
         movement = GetComponent<MovementComponent>();
         jump = GetComponent<JumpComponent>();
         gravity = GetComponent<GravityComponent>();
+        camComponent = GetComponent<CameraController>();
+
+        cam = Camera.main;
+    }
+
+    void Start() {
+        camComponent.CaptureMouse();
     }
 
     void Update() {
+        UpdateMovement();
+        UpdateCamera();
+        UpdateWeapon();
+    }
+
+    private void UpdateMovement() {
         if (controller.isGrounded && jumpInput.action.WasPressedThisFrame()) {
             jump.Jump(velocity);
         }
@@ -40,6 +56,29 @@ public class Player : MonoBehaviour {
         finalMove += movement.GetMovementDirection(transform, movementInput.action.ReadValue<Vector2>());
 
         controller.Move(finalMove * Time.deltaTime);
+    }
+
+    private void UpdateWeapon() {
+        if (lightAttackInput.action.ReadValue<float>() > 0f) {
+            Debug.Log("Light attack");
+        } else if (heavyAttackInput.action.ReadValue<float>() > 0f) {
+            Debug.Log("Heavy attack");
+        }
+
+        if (weaponSwapInput.action.WasPressedThisFrame()) {
+            Debug.Log("Swap weapon");
+        }
+    }
+
+    private void UpdateCamera() {
+        camComponent.RotateCamera(transform, cam, lookInput.action.ReadValue<Vector2>());
+    }
+
+    void OnDrawGizmos() {
+        if (cam != null) {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(cam.transform.position, cam.transform.forward * 10f);
+        }
     }
 }
 
