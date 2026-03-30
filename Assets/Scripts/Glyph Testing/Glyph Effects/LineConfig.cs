1391b461f9f7f2ea56712b5a996442bb576fc1d6
Liz Thompson <149344267+roiiLiz@users.noreply.github.com>
2026-03-29 23:12:33 -0400

    (feat) Slahes now get sent in the direction of your drawn line, rather than just the middle of the screen

--- Assets/Scripts/Glyph Testing/Glyph Effects/LineConfig.cs
+++ Assets/Scripts/Glyph Testing/Glyph Effects/LineConfig.cs
@@ -6,10 +6,6 @@ public class LineConfig : GlyphConfig {
     Camera cam;
 
     public override void Use(GlyphData glyphData) {
-        // Vector2 center = GlyphMatcher.GetCenter(glyphData.points);
-        // center.Scale(new Vector2(Screen.width / 1920f, Screen.height / 1080f));
-        // Vector3 viewport = Camera.main.ScreenToViewportPoint(center);
-        // Ray ray = Camera.main.ViewportPointToRay(viewport);
 
         cam = Camera.main;
 
@@ -17,15 +13,21 @@ public class LineConfig : GlyphConfig {
         float y = glyphData.points[glyphData.points.Count - 1].y - glyphData.points[0].y;
         float x = glyphData.points[glyphData.points.Count - 1].x - glyphData.points[0].x;
 
-        // Quaternion rot = Quaternion.Euler(0f, 0f, Mathf.Atan2(y, x) * Mathf.Rad2Deg);
-        // Quaternion newRot = rot * Quaternion.Euler(cam.transform.position + cam.transform.forward);
+        Vector2 center = GlyphMatcher.GetCenter(glyphData.points);
+        center.Scale(new Vector2(Screen.width / 1920f, Screen.height / 1080f));
+        Vector3 viewport = Camera.main.ScreenToViewportPoint(center);
+        Ray ray = Camera.main.ViewportPointToRay(viewport);
+
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.purple, 10f);
 
         GameObject go = GameObject.Instantiate(
             slashPrefab,
             cam.transform.position,
-            cam.transform.rotation
+            // cam.transform.rotation
+            cam.transform.rotation * Quaternion.Euler(ray.origin + ray.direction)
         );
 
+        go.transform.LookAt(ray.origin + ray.direction);
         go.transform.Rotate(new Vector3(0f, 0f, Mathf.Atan2(y, x) * Mathf.Rad2Deg));
     }
 }
